7597d226a7455fa4e746d6d341b312424a8cb267
VeyselKaraali <veysel-karaali@outlook.de>
2024-01-05 05:18:17 +0100

    Created Database Object

--- chatApp/chat/views.py
+++ chatApp/chat/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import Chat, Message
 def index(request):
   if request.method == 'POST':
     print("Received data " + request.POST["textMessage"])
+    myChat = Chat.objects.get(id=1)
+    Message.objects.create(text=request.POST["textMessage"], chat=myChat, author=request.user, receiver=request.user)
   return render(request, 'chat/index.html', {'username': 'Admin'})
