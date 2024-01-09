4916e7cb0c17fe420b9a2b71b737185e571ee99b
VeyselKaraali <veysel-karaali@outlook.de>
2024-01-09 02:39:32 +0100

    WIP: Registration

--- chatApp/chat/views.py
+++ chatApp/chat/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from .models import Chat, Message
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.core.validators import validate_email
 
 @login_required(login_url='/login/') #redirect when user is not logged in
 def index(request):
@@ -24,3 +26,67 @@ def login_view(request):
     else:
       return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
   return render(request, 'auth/login.html', {'redirect': redirect})
+
+def register(request):
+  form_data = {}
+  error_messages = {}
+  
+  if request.method == 'POST':
+    form_data['firstName'] = request.POST.get('firstName', '')
+    form_data['lastName'] = request.POST.get('lastName', '')
+    form_data['username'] = request.POST.get('username', '')
+    form_data['email'] = request.POST.get('email', '')
+    form_data['password'] = request.POST.get('password', '')
+    form_data['confirmPassword'] = request.POST.get('confirmPassword', '')
+    
+    error_messages['firstName'] = None
+    error_messages['lastName'] = None
+    error_messages['username'] = None
+    error_messages['email_invalid'] = None
+    error_messages['email_exists'] = None
+    error_messages['confirmPassword'] = None
+    
+    # Überprüfung, ob der Vorname nicht leer ist
+    if form_data['firstName'].strip() == '':
+      error_messages['firstName'] = 'Vorname darf nicht leer sein'
+      
+    # Überprüfung, ob der Nachname nicht leer ist
+    if form_data['lastName'].strip() == '':
+      error_messages['lastName'] = 'Nachname darf nicht leer sein'
+    
+    # Überprüfung, ob der Benutzername in der Datenbank existiert
+    if User.objects.filter(username__iexact = form_data['username']).exists():
+      error_messages['username'] = 'Dieser Benutzername existiert bereits'
+    
+    # Überprüfung, ob die E-Mail gültig ist
+    if validate_email(form_data['email']):
+      error_messages['email_invalid'] = 'Invalid E-mail'
+      
+    # Überprüfung, ob die E-Mail in der Datenbank existiert
+    if User.objects.filter(email__iexact=form_data['email']).exists():
+      error_messages['email_exists'] = 'Diese E-Mail existiert bereits'
+
+    # Überprüfung, ob die Passwörter übereinstimmen
+    if form_data['password'] != form_data['confirmPassword']:
+      error_messages['confirmPassword'] = 'Passwörter stimmen nicht überein'
+    
+    # Überprüfung, ob keine Validierungsfehler vorliegen
+    if all(value == None for value in error_messages.values()):
+        # Keine Validierungsfehler, Benutzer erstellen und authentifizieren
+        user = User.objects.create_user(
+            first_name=form_data['firstName'],
+            last_name=form_data['lastName'],
+            username=form_data['username'],
+            email=form_data['email'],
+            password=form_data['password']
+        )
+        
+        authenticated_user = authenticate(
+            username=form_data['username'],
+            password=form_data['password']
+        )
+        
+        if authenticated_user:
+          login(request, authenticated_user)
+          return HttpResponseRedirect(request.POST.get('/chat/'))
+  return render(request, 'auth/register.html', {'form_data': form_data, 'error_messages': error_messages})
